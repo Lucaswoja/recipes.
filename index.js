f3ed0cf5511c0e94b907cd1d26f135a613c119d4
Lucas <lucaswoja@gmail.com>
2022-12-21 00:36:05 -0300

    config index, routes and controller

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("olá");
+const express = require("express");
+const path = require("path"); // importa o Path
+const routes = require("./routes/routes"); //importa as rotas
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public"))); //identifica que os arquivos dos projetos estão na pasta public, css/js etc
+app.use(routes);
+app.listen(port, () =>
+  console.log(`Server Running in http://localhost:${port}`)
+);
